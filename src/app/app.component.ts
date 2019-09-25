bd9ea1d7aa53954a66ea55309d08ccc5e2a21551
Lucky686 <Lakshmi.durga990@gmail.com>
2019-09-25 09:53:45 +0530

    changes to appcomponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,36 +3,36 @@ import { Role } from '../app/role-categories/role-component';
 import { Router } from '@angular/router';
 import { AuthService } from './auth.service';
 @Component({
+    // tslint:disable-next-line: component-selector
     selector: 'my-app',
     templateUrl: './app.component.html',
 })
 export class AppComponent {
-    name = 'Angular';
 
     constructor(private router: Router, private authService: AuthService) {
 
     }
-    ngOnInit() {  this.getLinks(); }
-    //roles: Role[];
-    //roles = Roles;
+    name = 'Angular';
+    // roles: Role[];
+    // roles = Roles;
     selectedRole: Role;
     roles: Role[];
     links: any[];
     selectedCategory: Role;
 
-    isHidden: boolean = true;
+    isHidden = true;
+    // tslint:disable-next-line: use-lifecycle-interface
+    ngOnInit() {  this.getLinks(); }
 
     onSelect(role: Role): void {
 
         this.selectedRole = role;
-
     }
     OnSelectCategory(roleCategory: Role): void {
         this.selectedCategory = roleCategory;
 
     }
-  
     getLinks(): void {
-        this.authService.getLinks('getAllLinks').subscribe(links => { this.links = links.collection });
+        this.authService.getLinks('getAllLinks').subscribe(links => { this.links = links.collection; });
     }
 }
